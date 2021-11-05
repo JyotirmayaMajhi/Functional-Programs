1d5fac73e394849a0d6957bd04111987a8649b5b
unknown <jyotirmayamajhi1998@gmail.com>
2021-11-05 17:35:09 +0530

    Added distance between 2 points

---
+++ src/com/bridgelabz/array/Distance.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.array;
+import java.util.Scanner;
+public class Distance {
+
+	
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		System.out.println("x");
+		int x=0;
+		x = sc.nextInt();
+		
+		System.out.println("y");
+		int y=0;
+		y = sc.nextInt();
+	
+		int [][] arr = new int[x][y];
+		
+		double distance = Math.sqrt((x*x)+(y*y));
+		System.out.println("Distance from (0, 0) to (" + x + ", " + y + ") is " +distance);
+                
+                
+	}
+
+}
